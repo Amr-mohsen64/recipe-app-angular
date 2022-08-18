e7adf8587135354d749ebe82bb5dd9a0ef8e3ad8
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-08-18 12:47:58 +0200

    using cross component service to emit and subscribe to  selected recipe globaly

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Recipe } from "./recipe.model";
+import { Component, OnInit } from "@angular/core";
+import { RecipeService } from "./recipe.service";
 
 @Component({
-  selector: 'app-recipes',
-  templateUrl: './recipes.component.html',
-  styleUrls: ['./recipes.component.css']
+  selector: "app-recipes",
+  templateUrl: "./recipes.component.html",
+  styleUrls: ["./recipes.component.css"],
 })
 export class RecipesComponent implements OnInit {
-
-  constructor() { }
+  selectedRecipe: Recipe;
+  constructor(private recipeService: RecipeService) {}
 
   ngOnInit(): void {
+    this.recipeService.recipeSelected.subscribe(
+      (recipe) => (this.selectedRecipe = recipe)
+    );
   }
-
 }
