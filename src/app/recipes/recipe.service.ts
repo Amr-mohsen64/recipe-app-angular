e66a0fbb693caee046022a155b7ff0ac995f3a3f
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-10-09 00:47:24 +0200

    transforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,21 +9,22 @@ import { Subject } from "rxjs";
 })
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
-
-  private recipes: Recipe[] = [
-    new Recipe(
-      "Big Fat burger",
-      " A super tastey big fat Big Fat burger",
-      "https://www.bbcgoodfoodme.com/wp-content/uploads/2022/05/sticky-ginger-honey-chicken-skewers-with-noodle-salad.png",
-      [new Ingredient("meat", 1), new Ingredient("frech fries", 3)]
-    ),
-    new Recipe(
-      "KFC",
-      "A spicy KFC",
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
-      [new Ingredient("bread", 2), new Ingredient("meat", 3)]
-    ),
-  ];
+  private recipes: Recipe[] = [];
+  
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "Big Fat burger",
+  //     " A super tastey big fat Big Fat burger",
+  //     "https://www.bbcgoodfoodme.com/wp-content/uploads/2022/05/sticky-ginger-honey-chicken-skewers-with-noodle-salad.png",
+  //     [new Ingredient("meat", 1), new Ingredient("frech fries", 3)]
+  //   ),
+  //   new Recipe(
+  //     "KFC",
+  //     "A spicy KFC",
+  //     "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png",
+  //     [new Ingredient("bread", 2), new Ingredient("meat", 3)]
+  //   ),
+  // ];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Recipe } from "./../recipes/recipe.model";
 import { RecipeService } from "./../recipes/recipe.service";
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -23,6 +24,16 @@ export class DataStorageService {
     this.http
       .get<Recipe[]>(
         "https://ng-course-recipe-book-f7698-default-rtdb.firebaseio.com/recipes.json"
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
       )
       .subscribe((recipes) => this.recipeService.setRecipes(recipes));
   }
